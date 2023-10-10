38f582e6a421f24e4ecdc2b8e1b7274edcb10cf5
NishanSathsara <nishanksathsara@gmail.com>
2023-10-10 17:25:04 +0530

    context api

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import {Navbar,Footer, Sidebar, ThemeSettings} from './components';
 import {Ecommerce, Orders, Calendar, Employees,Stacked, Pyramid,Customers,Kanban, Area, Bar, Financial,ColorPicker,ColorMapping,Editor, Line, Pie} from './pages'
 
+import {  useStateContext } from "./context/contextProvider";
+
 import "./App.css";
 
 const App = () => {
-  const activeMenu = true;
+  const {activeMenu} = useStateContext();
 
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 import "./index.css";
+import { ContextProvider } from "./context/contextProvider";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <ContextProvider>
+        <App />,
+    </ContextProvider>,
+ document.getElementById("root")
+ );
