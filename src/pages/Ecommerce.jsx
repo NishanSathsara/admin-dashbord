4e097ec6a92ae4bf7c545d67e07feb35edae1065
NishanSathsara <nishanksathsara@gmail.com>
2023-10-12 03:38:16 +0530

    Revenue section with Syncfution charts not Working

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BsCurrencyDollar} from 'react-icons/bs';
-import {GoPrimitiveDot} from 'react-icons/go';
-import { Stacked, Pie, Button, SparkLine, } from '../components';
+import { GoDotFill } from 'react-icons/go';
+import { Stacked, Pie, Button, LineChart, SparkLine } from '../components';
 import { earningData,SparklineAreaData, ecomPieChartData } from '../data/dummy';
 import { useStateContext } from '../context/contextProvider';
 
@@ -52,9 +52,66 @@ const Ecommerce = () => {
           ))}
         </div>
       </div>
-      
+
+      <div className=" flex gap-10 flex-wrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
+          <div className="flex justify-between">
+            <p className="font-semibold text-xl">Revenue Updates</p>
+            <div className="flex items-center gap-4">
+              <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
+                <span><GoDotFill/></span>
+                <span>Expense </span>
+              </p>
+              <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
+                <span><GoDotFill/></span>
+                <span>Budget </span>
+              </p>
+            </div>
+          </div>
+          <div className=" mt-10 flex gap-10 flex-wrap justify-center">
+            <div className="border-r-1 border-color m-4 pr-10">
+              <div>
+                <p>
+                  <span className="text-3xl text-font-semibold">$95,000</span>
+                  <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">24%</span>
+                </p>
+                <p className="text-gray-500 mt-1">Budget</p>
+              </div>
+              <div className="mt-8">
+                <p>
+                  <span className="text-3xl text-font-semibold">$59,000</span>
+                </p>
+                <p className="text-gray-500 mt-1">Expense</p>
+              </div>
+              <div className="mt-5">
+                Fix the error
+                {/* <SparkLine 
+                currentColor="blue" 
+                id="line-sparkLine" 
+                type="Line" 
+                height="80px" 
+                width="250px" 
+                data={SparklineAreaData} 
+                color="blue" /> */}
+              </div>
+              <div className="mt-10">
+                <Button
+                  color="white"
+                  bgColor="blue"
+                  text="Download Report"
+                  borderRadius="10px"
+                />
+              </div>
+            </div>
+            <div>
+              Fix the error
+              {/* <Stacked width="320px" height="320px"/> */}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Ecommerce
+export default Ecommerce;
